65628ebae41c475ad212d1bf78d97b3a86e01551
hygxsh2k <42065600+hygxsh2k@users.noreply.github.com>
2019-04-07 08:47:18 +0900

    Add files via upload

---
+++ sort.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+[ -z "$(ls -AUCT0)" ] && exit 0
+while [ "./$(ls -AU | tail -1)" != "$temp" ]
+do
+	temp=$(mktemp -p . XXXX)
+	temps="$temps $temp"
+done
+tempdir=$(mktemp -d -p . tempdir.XXXX)
+rm $temps
+mv $(ls -AU | head -n-1) $tempdir
+cd $tempdir
+mv $(ls -A) ..
+cd ..
+rmdir $tempdir
