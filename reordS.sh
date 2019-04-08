49b78bd73883de73f90587f91d248534b90cf6f6
hygxsh2k <hygxsh2k@gmail.com>
2019-04-08 23:37:33 +0900

    Update 'reordS.sh'.

--- reordS.sh
+++ reordS.sh
@@ -1,14 +1,11 @@
-#!/bin/sh
+#!/bin/sh -x
 
-# '.reord_SAVED'で指定したファイル順に先頭から配置後、
-# 残りのファイルをアルファベット順に再配置する。
-# '.reord_SAVED'には'ls -fl --fu'相当のデータを含めると
-# データ保全に役立つ。
+# '.reord_SAVED'で指定したファイル順に先頭から配置後、残りのファイルをアルファ
+# ベット順に配置する。'.reord_SAVED'を'ls -fl --fu'コマンドの出力形式で記述して
+# ファイルを管理しておくとデータの保全に役立つ。
+
 [ ! -w '.' ] && exit 1
-[ ! -s '.reord_SAVED' ] && exit 2
-[ -n "$(grep -x -F '.' .reord_SAVED)" ] && exit 3
-[ -n "$(grep -x -F '..' .reord_SAVED)" ] && exit 3
-[ -n "$(grep -x -F '.reord_SAVED' .reord_SAVED)" ] && exit 3
+[ ! -r '.reord_SAVED' ] && exit 2
 [ -z "$(ls -AUCT0)" ] && exit 0
 while [ "./$(ls -AU | tail -1)" != "$temp" ]
 do
@@ -17,12 +14,13 @@ do
 done
 tempdir=$(mktemp -d -p . tempdir.XXXX)
 rm $temps
-[ -s '.reord_SAVED' ] && mv $(awk '{print $NF}' .reord_SAVED) $tempdir
+saved=$(awk '{print $NF}' .reord_SAVED | grep -Fxv '.' | grep -Fxv '..' | grep -Fxv '.reord_SAVED')
+[ -n "$saved" ] && mv $saved $tempdir
 mv .reord_SAVED $tempdir
 [ -n "$(ls -AU | head -n-1)" ] && mv $(ls -AU | head -n-1) $tempdir
 cd $tempdir
-mv $(awk '{print $NF}' .reord_SAVED) ..
+[ -n "$saved" ] && mv $saved ..
 mv .reord_SAVED ..
-mv $(ls -A) ..
+[ -n "$(ls -A)" ] && mv $(ls -A) ..
 cd ..
 rmdir $tempdir
